ca9815ea2dfa924f822e9568aac4498a7132655c
Farshid Roohi <Farshid.roohi.a@Gmail.com>
2017-12-20 10:52:42 +0330

    clean code add comment to code

--- views/src/main/java/com/zarinpal/libs/views/SectionEditText.java
+++ views/src/main/java/com/zarinpal/libs/views/SectionEditText.java
@@ -10,13 +10,11 @@ import android.text.InputFilter;
 import android.text.InputType;
 import android.text.TextWatcher;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -81,63 +79,36 @@ public class SectionEditText extends LinearLayout {
     private void addView() {
 
         for (int i = 0; i <= this.itemCount; i++) {
+            final int index = i;
+            // initialize View Item
             RelativeLayout relativeLayout = new RelativeLayout(getContext());
             final ZarinEditText editText = new ZarinEditText(getContext());
             final MaterialEditText edt = editText.getEditText();
-            editText.setTag(i);
-
-            InputFilter[] filters = new InputFilter[1];
-            filters[0] = new InputFilter.LengthFilter(this.maxLen);
-            edt.setFilters(filters);
-            edt.setGravity(Gravity.CENTER);
-            edt.setTextColor(this.textColor);
-            // handled background user
             final ImageView imageView = new ImageView(getContext());
 
-            if (this.backgroundDrawable != null) {
-                imageView.setBackground(this.backgroundDrawable);
-                RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(40, 2);
-                params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
-                imageView.setLayoutParams(params);
-                edt.setBackgroundDrawable(null);
-                relativeLayout.addView(imageView);
+            // set (Max length , Color Hint , Gravity , TextColor , inputType,action keyboard) ->  To Edit Text
+            this.setAttrEditText(edt, index);
 
-            }
 
-            // check final item for done action
-            if (i == itemCount) {
-                edt.setImeOptions(EditorInfo.IME_ACTION_DONE);
-            } else {
-                edt.setImeOptions(EditorInfo.IME_ACTION_NEXT);
-            }
+            // handled background user
+            this.handledBackground(edt, relativeLayout, imageView);
 
-            // Check User input Password
-            if (this.hasPassword) {
-                edt.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
-            } else {
-                edt.setInputType(InputType.TYPE_CLASS_TEXT);
-            }
 
-            // set item size
-            LayoutParams params = new LayoutParams((int) UnitUtility.dpToPx(getContext(), this.itemWidth)
-                    , (int) UnitUtility.dpToPx(getContext(), this.itemHeight));
-
-            params.setMargins(this.itemMargin, this.itemMargin, this.itemMargin, this.itemMargin);
-
-            relativeLayout.addView(editText, params);
-
-            this.layoutRoot.addView(relativeLayout);
+            // Add EditText and getParam()  -> {size , margin}
+            relativeLayout.addView(editText, getParam());
 
             // add view to root layout
+            this.layoutRoot.addView(relativeLayout);
             this.editTextList.add(editText);
-            final int index = i;
 
+
+            // Check focus EditText for Visibility ImageView
             edt.setOnFocusChangeListener(new OnFocusChangeListener() {
                 @Override
-                public void onFocusChange(View view, boolean b) {
-                    if (backgroundDrawable != null && b) {
+                public void onFocusChange(View view, boolean isFocused) {
+                    if (backgroundDrawable != null && isFocused) {
                         imageView.setVisibility(GONE);
-                    } else if (edt.getText().toString().isEmpty()){
+                    } else if (edt.getText().toString().isEmpty()) {
                         imageView.setVisibility(VISIBLE);
                     }
                 }
@@ -155,22 +126,80 @@ public class SectionEditText extends LinearLayout {
 
                 @Override
                 public void afterTextChanged(Editable editable) {
-                    String value = editable.toString();
 
-                    if ((value.isEmpty()) && (index != 0)) {
-                        editTextList.get((index - 1)).requestFocus();
-                    }
-
-                    if ((value.isEmpty()) || (value.length() < maxLen)) {
-                        return;
-                    }
-
-                    if (editTextList.size() > (index + 1)) {
-                        editTextList.get(index + 1).requestFocus();
-                    }
-
+                    handledUserType(editable.toString(), index);
                 }
             });
+        }
+    }
+
+    private void handledUserType(String value, int index) {
+
+        // check if user input empty And not first Item requested last Item focus
+        if ((value.isEmpty()) && (index != 0)) {
+            editTextList.get((index - 1)).requestFocus();
+        }
+
+        // check if user input empty Or  user input length fewer max Length return
+        if ((value.isEmpty()) || (value.length() < maxLen)) {
+            return;
+        }
+
+        // check EditText ArrayList Size Bigger off (index + 1) requested next Item focus
+        if (editTextList.size() > (index + 1)) {
+            editTextList.get(index + 1).requestFocus();
+        }
+    }
+
+    private LayoutParams getParam() {
+
+        // set Size Item , Margin
+        LayoutParams params = new LayoutParams((int) UnitUtility.dpToPx(getContext(), this.itemWidth)
+                , (int) UnitUtility.dpToPx(getContext(), this.itemHeight));
+
+        params.setMargins(this.itemMargin, this.itemMargin, this.itemMargin, this.itemMargin);
+
+        return params;
+    }
+
+    private void handledBackground(MaterialEditText edt, RelativeLayout layout, ImageView img) {
+        // Check Exist Drawable Background for EditText
+        if (this.backgroundDrawable != null) {
+            img.setBackground(this.backgroundDrawable);
+            // set height , width imageView
+            RelativeLayout.LayoutParams params =
+                    new RelativeLayout.LayoutParams((int) UnitUtility.dpToPx(getContext(),
+                            30), (int) UnitUtility.dpToPx(getContext(), 2));
+            params.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+            img.setLayoutParams(params);
+            edt.setBackgroundDrawable(null);
+            layout.addView(img);
+        }
+    }
+
+    private void setAttrEditText(MaterialEditText edt, int index) {
+
+        // set Max length To Edit Text
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter.LengthFilter(this.maxLen);
+
+        // set Color Hint And Gravity , TextColor
+        edt.setFilters(filters);
+        edt.setGravity(Gravity.CENTER);
+        edt.setTextColor(this.textColor);
+
+        // Check User input Password
+        if (this.hasPassword) {
+            edt.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        } else {
+            edt.setInputType(InputType.TYPE_CLASS_TEXT);
+        }
+
+        // set Action soft keyboard
+        if (index == itemCount) {
+            edt.setImeOptions(EditorInfo.IME_ACTION_DONE);
+        } else {
+            edt.setImeOptions(EditorInfo.IME_ACTION_NEXT);
         }
     }
 }
