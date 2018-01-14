2377a260c14ec560d0e08fd428c83c5cd4c8dfb6
hossein-amini <hosein_amini@ymail.com>
2018-01-14 16:02:10 +0330

    Fix bug in ZarinButton and add setBackground method for it  Ver 0.2.1

--- app/src/main/java/com/zarinpal/views/MainActivity.java
+++ app/src/main/java/com/zarinpal/views/MainActivity.java
@@ -3,6 +3,8 @@ package com.zarinpal.views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.zarinpal.libs.views.ZarinButton;
+
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
@@ -11,6 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ZarinButton btn = findViewById(R.id.btn);
+
+        btn.setBackgroundColor(getResources().getColor(R.color.colorPrimary),
+                getResources().getColor(R.color.colorAccent));
 
     }
 
