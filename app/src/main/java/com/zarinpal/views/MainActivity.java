396323f93be4629c34d84e8db971e842001fe539
Farshid Roohi <Farshid.roohi.a@Gmail.com>
2018-01-14 10:46:18 +0330

    fix key handled empty editText

--- app/src/main/java/com/zarinpal/views/MainActivity.java
+++ app/src/main/java/com/zarinpal/views/MainActivity.java
@@ -2,33 +2,15 @@ package com.zarinpal.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-
-
-import com.zarinpal.libs.views.ZarinEditText;
-import com.zarinpal.libs.views.ZarinTextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean condition = true;
-    ZarinEditText zarinEditText;
+    public static final String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        zarinEditText = findViewById(R.id.edt_zarin);
-        final ZarinTextView txtTest = findViewById(R.id.txt_test);
-       //zarinEditText.setType(ZarinEditTextType.PAN);
-
-
-        zarinEditText.setOnTextChangeListener(new ZarinEditText.OnTextChangeListener() {
-            @Override
-            public void onTextChange(String val) {
-                Log.d("TEXTTTTTTTT", "onTextChange: ");
-                txtTest.setText(val);
-            }
-        });
 
     }
 
