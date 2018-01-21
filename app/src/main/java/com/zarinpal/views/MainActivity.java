ce951421a3a7dd621de35a95c5bbbc22e5e5dd4e
hossein-amini <hosein_amini@ymail.com>
2018-01-21 10:49:36 +0330

    Create copyTextIntoClipboard for ZarinTextView and Ver 0.3.2

--- app/src/main/java/com/zarinpal/views/MainActivity.java
+++ app/src/main/java/com/zarinpal/views/MainActivity.java
@@ -3,9 +3,12 @@ package com.zarinpal.views;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.zarinpal.libs.views.ZarinButton;
 import com.zarinpal.libs.views.ZarinPinView;
+import com.zarinpal.libs.views.ZarinTextView;
+import com.zarinpal.libs.views.utitlity.TextUtility;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +19,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ZarinButton btn = findViewById(R.id.btn);
+        ZarinButton         btn     = findViewById(R.id.btn);
+        final ZarinTextView txtTest = findViewById(R.id.txt_test);
 
         btn.setBackgroundColors(getResources().getColor(R.color.colorPrimary),
                 getResources().getColor(R.color.colorAccent));
@@ -29,6 +33,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        
+        txtTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtTest.copyTextIntoClipboard(null);
+            }
+        });
 
     }
 
