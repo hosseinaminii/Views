9ce0260b9f6fd04da3eb40fa9e084832304d1bfb
Farshid Roohi <Farshid.roohi.a@Gmail.com>
2018-01-22 15:56:35 +0330

    fix timer class name and fix shit bg progressBar

--- app/src/main/java/com/zarinpal/views/MainActivity.java
+++ app/src/main/java/com/zarinpal/views/MainActivity.java
@@ -1,16 +1,10 @@
 package com.zarinpal.views;
 
-import android.graphics.Color;
-import android.graphics.drawable.ShapeDrawable;
-import android.graphics.drawable.shapes.OvalShape;
-import android.support.annotation.ColorInt;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.zarinpal.libs.views.ZarinButton;
-import com.zarinpal.libs.views.ZarinItemView;
-import com.zarinpal.libs.views.ZarinPinView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,31 +19,6 @@ public class MainActivity extends AppCompatActivity {
 
         btn.setBackgroundColors(getResources().getColor(R.color.colorPrimary),
                 getResources().getColor(R.color.colorAccent));
-
-        ZarinPinView zarin_pin = findViewById(R.id.zarin_pin);
-        zarin_pin.setOnPinEnteredListener(new ZarinPinView.OnPinEnteredListener() {
-            @Override
-            public void onPinEntered(CharSequence str) {
-                Log.i(TAG, "onPinEntered: " + str);
-            }
-        });
-
-        final ShapeDrawable oval = new ShapeDrawable(new OvalShape());
-        oval.setIntrinsicHeight(200);
-        oval.setIntrinsicWidth(200);
-        oval.setAlpha(30);
-        oval.getPaint().setColor(Color.BLUE);
-
-        ZarinItemView item = findViewById(R.id.item);
-        item.setIconBg(oval);
-        item.setEnabled(false);
-        item.setListener(new ZarinItemView.OnClickItemListener() {
-            @Override
-            public void onClickItem() {
-                Log.i(TAG, "onClickItem: ");
-            }
-        });
-
 
     }
 
